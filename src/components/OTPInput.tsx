d9c81cc58ce53cd300ed4a9ace424e6d98199e60
bn-madhu <madhugowda.588@gmail.com>
2025-05-04 13:43:53 +0530

    fixed otp

--- src/components/OTPInput.tsx
+++ src/components/OTPInput.tsx
@@ -1,8 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const OTPInput = ({ length = 4, onOtpSubmit }) => {
   const [otp, setOtp] = useState(new Array(length).fill(""));
   const inputRefs = useRef<Array<HTMLInputElement>>([]);
+
+  useEffect(() => {
+    if (inputRefs.current[0]) {
+      inputRefs.current[0].focus();
+    }
+  }, []);
 
   const handleChange = (index: number, e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -11,8 +17,21 @@ const OTPInput = ({ length = 4, onOtpSubmit }) => {
     // allow only one input
     newOtp[index] = value.substring(value.length - 1);
     setOtp(newOtp);
+    if(inputRefs.current[index + 1] && index < length-1 && value){
+      inputRefs.current[index+1].focus();
+    }
+
+    // submit trigger
+    const combinedOtp = newOtp.join("");
+    if (combinedOtp.length === length) onOtpSubmit(combinedOtp);
   }
-  const handleClick = (index: number) => {}
+  const handleClick = (index: number) => {
+    inputRefs.current[index].setSelectionRange(1, 1);
+
+    if (index > 0 && !otp[index - 1]) {
+      inputRefs.current[otp.indexOf("")].focus();
+    }
+  }
   const handleKeyDown = (index: number, e: React.ChangeEvent) => {}
   return (
     <div className='otpInput-container'>

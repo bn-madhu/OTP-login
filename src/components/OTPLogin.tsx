f4e068d8141cdc83d44174e3f5afc7e22fc39221
bn-madhu <madhugowda.588@gmail.com>
2025-05-06 18:17:47 +0530

    Added dog svg, added close model popup

--- src/components/OTPLogin.tsx
+++ src/components/OTPLogin.tsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 import Button from './assets/Button'
 import OTPInput from './OTPInput';
 import PopupModel from './assets/Popup-model';
+import { LoginDog } from './assets/icons';
 
 const OTPLogin = () => {
 
+  const [userName, setUserName] = useState("");
   const [mobileNumber, setMobileNumber] = useState("");
   const [showOtpInput, setShowOtpInput] = useState(false);
+  const [showPopUpModel, setShowPopUpModel] = useState(false);
 
   const handelSubmit = () => {
     const regex = /[^0-9]/g;
     if (mobileNumber.length < 10 || regex.test(mobileNumber)) {
       // alert("Invalid Phone Number");
+      setShowPopUpModel(true);
       return;
     }
     // Call BE API
@@ -19,38 +23,58 @@ const OTPLogin = () => {
     setShowOtpInput(true);
   }
 
-  const onOtpSubmit = (otp:string) => {
+  const onOtpSubmit = (otp: string) => {
     console.log("Login Successful", otp);
   };
 
   return (
-    <div className='w-screen h-full text-center'>
-      <h3 className="text-3xl font-bold underline">OTP UI</h3>
+    <div className='h-full xl:max-w-[1200px] lg:max-w-[1024px] md:max-w-[786px] sm:max-w-[300px] mx-auto'>
+      <LoginDog className="w-40 h-40" />
       {/* Enter mobile form area */}
-      {!showOtpInput ? <>
-        <div className='form-control flex items-center justify-center gap-4 my-4 mx-auto max-w-dvw w-full'>
-          <input
-            type="text"
-            required
-            value={mobileNumber}
-            placeholder='Enter mobile number'
-            maxLength={10}
-            onChange={(e) => setMobileNumber(e.target.value)}
-            className='border border-gray-600 p-2 rounded'
-          />
+      <div>
+        {!showOtpInput ? <>
+          <div className='form-control my-4 mx-auto max-w-dvw w-full'>
+            <label htmlFor='user-name'>Name</label>
+            <input
+              type="text"
+              name="user-name"
+              id='user-name'
+              required
+              value={userName}
+              placeholder='Your name'
+              maxLength={10}
+              onChange={(e) => setUserName(e.target.value)}
+              className='border border-gray-600 p-2 rounded'
+            />
+          </div>
+          <div className='form-control my-4 mx-auto max-w-dvw w-full'>
+            <label htmlFor='mobileNum'>Mobile or Email address</label>
+            <input
+              type="text"
+              name="mobileNum"
+              id='mobileNum'
+              required
+              value={mobileNumber}
+              placeholder='Enter mobile number'
+              maxLength={10}
+              onChange={(e) => setMobileNumber(e.target.value)}
+              className='border border-gray-600 p-2 rounded'
+            />
+          </div>
           <Button onClick={handelSubmit}>Submit</Button>
-        </div>
-        {true && <PopupModel>
-          <div className="model-content text-left">Please provide the correct Mobile/Phone number</div>
+          {showPopUpModel && <PopupModel>
+            <p className="model-content text-left mb-4">Please provide the correct Mobile/Phone number</p>
+            <Button variant='outline' className='float-left' onClick={() => setShowPopUpModel(true)}>Close</Button>
           </PopupModel>}
-      </>
-        :
-        <div className="otp-form">
-          {/* If Mobile number is validated show otp */}
-          Enter OTP sent to {mobileNumber}
-          <OTPInput length={4} onOtpSubmit={onOtpSubmit} />
-        </div>
-      }
+        </>
+          :
+          <div className="otp-form">
+            {/* If Mobile number is validated show otp */}
+            Enter OTP sent to {mobileNumber}
+            <OTPInput length={4} onOtpSubmit={onOtpSubmit} />
+          </div>
+        }
+      </div>
     </div>
   )
 }
--- src/components/assets/Popup-model.tsx
+++ src/components/assets/Popup-model.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { CloseIcon } from './icons'
 
 const PopupModel = ({children}:any) => {
   return (
-    <div className='flex items-center justify-center fixed bg-gray-900 opacity-80 top-0 left-0 right-0 bottom-0'>
-        <div className="model-pop-up max-w-[60%] max-h-[60%] bg-white border border-gray-400 p-4 h-full w-full mx-auto rounded-lg">
-        {children}
+    <div className='flex items-center fixed bg-gray-900 opacity-80 top-0 left-0 right-0 bottom-0'>
+        <div className="model-pop-up max-w-[60%] max-h-[60%] w-full h-fit bg-white border border-gray-400 p-8 mx-auto rounded-lg relative">
+          <CloseIcon width="24px" height="24px" className="w-10 h-10 absolute top-0 right-0" />
+          {children}
         </div>
     </div>
   )

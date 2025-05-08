2c76c70a9bbde38c46b48ddf272483251847709f
bn-madhu <madhugowda.588@gmail.com>
2025-05-08 22:13:30 +0530

    fixed for deployment

--- src/components/OTPLogin.tsx
+++ src/components/OTPLogin.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import Button from './assets/Button'
 import OTPInput from './OTPInput';
 import PopupModel from './assets/Popup-model';
--- src/components/assets/icons.tsx
+++ src/components/assets/icons.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const CloseIcon = (props:any) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}><rect width="256" height="256" fill="none"/><line x1="160" y1="96" x2="96" y2="160" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><line x1="96" y1="96" x2="160" y2="160" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><circle cx="128" cy="128" r="96" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
